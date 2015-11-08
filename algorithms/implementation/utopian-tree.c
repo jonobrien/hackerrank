deb792a2c47c7942c79a8a25fa7f087975886390
Jon O'Brien <jvo7822@rit.edu>
2015-11-08 00:51:10 -0500

    fixed spacing

--- algorithms/implementation/utopian-tree.c
+++ algorithms/implementation/utopian-tree.c
@@ -5,29 +5,29 @@
 #include <stdlib.h>
 
 int uTree(int n) {
-  int i = 1;
-  int height = 1;
-
-  for(i; i <= n; i++) {
-    if (i&1) {
-      height = height << 1;
-    }
-    else {
-      height += 1;
-    }
-  }
-  return height;
+	int i = 1;
+    int height = 1;
+    
+	for(i; i <= n; i++) {
+		if (i&1) {
+			height = height << 1;
+        }
+        else {
+			height += 1;
+        }
+	}
+	return height;
 }
 
 
 int main() {
-  int T;
-  scanf("%d",&T);
-  int N;
-  int i = 0;
-  for(i; i < T; i++) {
-    scanf("%d",&N);
-    printf("%d\n", uTree(N));
-  }
-  return 0;
+	int T;
+	scanf("%d",&T);
+    int N;
+    int i = 0;
+	for(i; i < T; i++) {
+		scanf("%d",&N);
+		printf("%d\n", uTree(N));
+	}
+	return 0;
 }
